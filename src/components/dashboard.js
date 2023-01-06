7cc235bc2035d82ea017ff608b13742d5a4540aa
Prateek <prateekag.998@gmail.com>
2023-01-06 17:18:00 +0530

    added filter functionality in user dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -14,6 +14,9 @@ const Dashboard = (props) => {
 
     //to fetch details of all the restaurants
     const restaurants = JSON.parse(localStorage.getItem('restaurants'));
+    
+    //to store the filtered restaurants
+    const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants);
 
     //will get called when user will select any restaunt
     const onSelectRestaurant = (name) => {
@@ -25,16 +28,28 @@ const Dashboard = (props) => {
         })
     }
 
+    //to filter the restaurants with name
+    const onSearchByName = (event) => {
+        setFilteredRestaurants(restaurants.filter((restaurant)=>{
+            if(restaurant.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1){
+                return true;
+            }
+            return false;
+        }))
+    }
+
     //will get called when user wants to open or close the modal which contains restarant details
     const toggleModalHandler = (e) => {
         setOpenDetail((prevValue)=>!prevValue);
     }
     
     return <div className={styles['dashboard-container']}>
-        {restaurants?.map((element, index)=><div className={styles['res-card']} key={index} onClick={()=>{onSelectRestaurant(element.name)}}>
+        <input type='text' className={styles['search-box']} placeholder='Enter Search Name' onChange={onSearchByName}/>
+        {filteredRestaurants?.map((element, index)=><div className={styles['res-card']} key={index} onClick={()=>{onSelectRestaurant(element.name)}}>
             <p>Name: {element.name}</p>
             <p>Rating: {element.rating}</p>
         </div>)}
+        {filteredRestaurants.length === 0 && <h1>No Restaurants in your area</h1>}
         {openDetail && ReactDOM.createPortal(<Modal closeModal={toggleModalHandler} details={restaurantSelected}></Modal>, document.getElementById('modal-div'))}
     </div>
 }
